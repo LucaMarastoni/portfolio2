676d39852c962af2e8e73b36f8b25a17eee3bfea
LucaMarastoni <marastoni311@gmail.com>
2024-08-06 20:36:12 +0200

    k

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.getElementById("icon").addEventListener("click", function() {
     const works = document.getElementById("works");
-    if (works.classList.contains("show")) {
-        works.classList.remove("show");
-    } else {
-        works.classList.add("show");
-    }
+    works.classList.toggle("show");
 });
+
+
